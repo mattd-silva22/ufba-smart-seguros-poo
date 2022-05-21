272569d9507a785559a9e7b0c4ffdee7c2eb0640
Lavio <laviovale@gmail.com>
2022-05-21 13:30:56 -0300

    feat: add form de vendedor e lista de clientes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const aside = document.querySelector("aside");
+const mainSection = document.querySelector("#main-section");
 const checkboxMenu = document.querySelector("#checkbox-menu");
 const menuMobie = document.querySelector("#menu-mobile");
 const formLabels = [
@@ -9,6 +10,53 @@ const formLabels = [
   "Seguro de celular",
 ];
 
+const salersForm = `
+<form class="form-container">
+<p class="title">Cadastro de vendedores</p>
+<input type="text" placeholder="Nome" />
+<input type="text" placeholder="Email" />
+<p class="gender">Gênero</p>
+
+<input type="radio" id="gender_male" value="gender_male" />
+<label for="gender_male">Masculino</label>
+
+<input type="radio" id="gender_female" value="gender_female" />
+<label for="gender_female">Feminino</label>
+
+<input type="radio" id="other" value="other" />
+<label for="other">Outro</label>
+
+<input type="tel" placeholder="Telefone" />
+</form>
+`;
+
+const clientList = `
+<div class="list">
+  <p class="list-title">Lista de Clientes</p>
+  <div class="list-table">
+    <div class="list-table-header">
+      <p></p>
+      <p>Nome</p>
+      <p>Email</p>
+      <p>Telefone</p>
+      <p>Ações</p>
+    </div>
+    <div class="list-table-body" id="list-table-body">
+      <div class="list-table-row">
+        <p><img src="./assets/Group 6.svg" alt="user photo" /></p>
+        <p>Fulano</p>
+        <p>fulano@smart.com.br</p>
+        <p>(71)99999-0000</p>
+        <p class="list-actions">
+          <button class="edit-button">Editar</button>
+          <button>Excluir</button>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
 function populateAside() {
   formLabels.forEach((label) => {
     aside.innerHTML += `
@@ -17,6 +65,7 @@ function populateAside() {
         </div>
       `;
   });
+  aside.innerHTML += salersForm;
 }
 
 checkboxMenu.addEventListener("change", () => {
