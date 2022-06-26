40801b7aa00ba44c781aa01a73e98dd20d9302ae
Lavio <laviovale@gmail.com>
2022-06-26 16:46:17 -0300

    feat: add getters and setters for classes

--- src/js/model/ContratoSeguro.js
+++ src/js/model/ContratoSeguro.js
@@ -1,5 +1,67 @@
- class ContratoSeguro{
-    constructor(nomeCliente,cpfCliente,nomeVendedor,codVendedor,item,valorContrato){
-        
-    }
+class ContratoSeguro {
+  constructor(
+    nomeCliente,
+    cpfCliente,
+    nomeVendedor,
+    codVendedor,
+    item,
+    valorContrato
+  ) {
+    this._nomeCliente = nomeCliente;
+    this._cpfCliente = cpfCliente;
+    this._nomeVendedor = nomeVendedor;
+    this._codVendedor = codVendedor;
+    this._item = item;
+    this._valorContrato = valorContrato;
+  }
+
+  //getters
+  get nomeCliente() {
+    return this._nomeCliente;
+  }
+
+  get cpfCliente() {
+    return this._cpfCliente;
+  }
+
+  get nomeVendedor() {
+    return this._nomeVendedor;
+  }
+
+  get codVendedor() {
+    return this._codVendedor;
+  }
+
+  get item() {
+    return this._item;
+  }
+
+  get valorContrato() {
+    return this._valorContrato;
+  }
+
+  //setters
+  set nomeCliente(value) {
+    this._nomeCliente = value;
+  }
+
+  set cpfCliente(value) {
+    this._cpfCliente = value;
+  }
+
+  set nomeVendedor(value) {
+    this._nomeVendedor = value;
+  }
+
+  set codVendedor(value) {
+    this._codVendedor = value;
+  }
+
+  set item(value) {
+    this._item = value;
+  }
+
+  set valorContrato(value) {
+    this._valorContrato = value;
+  }
 }
--- src/js/model/Vendedor.js
+++ src/js/model/Vendedor.js
@@ -1,7 +1,25 @@
-export class Vendedor extends User{
-    constructor(nome,genero,email,dataRegistro,tipo,codVendedor,telefone){
-        super(nome,genero,email,dataRegistro,tipo)
-        this.codigoVendedor = codVendedor
-        this.telefone = telefone
-    }
+export class Vendedor extends User {
+  constructor(nome, genero, email, dataRegistro, tipo, codVendedor, telefone) {
+    super(nome, genero, email, dataRegistro, tipo);
+    this._codigoVendedor = codVendedor;
+    this._telefone = telefone;
+  }
+
+  //getters
+  get codigoVendedor() {
+    return this._codigoVendedor;
+  }
+
+  get telefone() {
+    return this._telefone;
+  }
+
+  //setters
+  set codigoVendedor(value) {
+    this._codigoVendedor = value;
+  }
+
+  set telefone(value) {
+    this._telefone = value;
+  }
 }
